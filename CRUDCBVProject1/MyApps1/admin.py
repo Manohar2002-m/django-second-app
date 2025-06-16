7bd505e0e401724882267d9ce8e22ff474204828
Manohar2002-m <manoharhardrik2002@gmail.com>
2025-06-16 12:34:27 +0530

    first

---
+++ CRUDCBVProject1/MyApps1/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+#from django.contrib import admin
+from MyApps1.models import Company
+#Register your models here.
+class CompanyAdmin(admin.ModelAdmin):
+	list_display=['name', 'location','ceo'];
+
+admin.site.register(Company,CompanyAdmin)
+
+
+
